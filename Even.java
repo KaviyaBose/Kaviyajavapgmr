9854d9cde84862c96a90c4d6d6d3a63d9bf56e55
KaviyaBose <108384992+KaviyaBose@users.noreply.github.com>
2022-07-13 18:56:56 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=50;
+		for(int i=1;i<=50;i++) {
+			if(i%2==0) {
+			
+		
+		System.out.println(i);
+	}
+
+}
+}
+}
