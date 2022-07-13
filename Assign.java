9c1109ca51c3d977ad6ef858a5f6bc3f5bc70cdb
KaviyaBose <108384992+KaviyaBose@users.noreply.github.com>
2022-07-13 18:48:34 +0530

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class Assign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=50;
+		for(int i=1;i<=50;i++) {
+			if(i%2!=0) {
+			
+		
+		System.out.println(i);
+		
+		}
+
+		}
+	}
+}
+
